8ef6dafa42860d3c4cc2de3c3fee2ff0967bc32e
23MHCQ05117 <147488425+23MHCQ05117@users.noreply.github.com>
2024-01-01 16:40:25 +0530

    #include<stdio.h> int main() {     int i,j,k,n,a;     scanf("%d",&n);     a=n;     for(i=1;i<=n;i++)     {         for(k=1;k<i;k++)         {             printf(" ");
            }
            for(j=a;j>=1;j--)
            {
            printf("%c ",64+i);
        }
        a--;
            
        printf("
    ");
        }
    }

---
+++ Pattern_6.5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k,n,a;
+    scanf("%d",&n);
+    a=n;
+    for(i=1;i<=n;i++)
+    {
+        for(k=1;k<i;k++)
+        {
+            printf(" ");
+        
+        }
+        for(j=a;j>=1;j--)
+        {
+        printf("%c ",64+i);
+    }
+    a--;
+        
+    printf("
+");
+    }
+}
